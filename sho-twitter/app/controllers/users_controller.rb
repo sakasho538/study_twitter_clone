134091537280eabb9cb90b6fdd02e412beb5e47c
sakashita <sakashita@1-10.com>
2018-08-14 00:47:54 +0900

    データベース関連

--- sho-twitter/app/controllers/users_controller.rb
+++ sho-twitter/app/controllers/users_controller.rb
@@ -3,17 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    if params[:username] == 'shos'
-      @user[:name] = 'Sho Sakashita'
-      @user[:username] = 'shos'
-      @user[:location] = 'Tokyo, Japan'
-      @user[:about] = 'Hello.'
-    elsif params[:username] == 'rannas'
-      @user[:name] = 'ranna saito'
-      @user[:username] = 'rannas'
-      @user[:location] = 'Chiba, Japan'
-      @user[:about] = 'I am ranna.'
-    end
+    @user = User.find_by(:username => params[:username])
   end
 end
--- sho-twitter/db/seeds.rb
+++ sho-twitter/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@user = User.new
+@user.name = 'Sho Sakashita'
+@user.username = 'shos'
+@user.location = 'Tokyo, Japan'
+@user.about = 'Hello, I am Sho.'
+@user.save
+
+@user = User.new
+@user.name = 'Ranna Saito'
+@user.username = 'rannas'
+@user.location = 'Chiba, Japan'
+@user.about = 'Hi, I am Ranna!'
+@user.save
